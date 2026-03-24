d8a5151c6ec260f76e635b0aa2db6a4b7eeb013e
Selva <slvssb08@gmail.com>
2026-03-24 13:38:16 +0530

    Initial Commit

---
+++ skills-ui/skills_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  skills_uiApp.swift
+//  skills-ui
+//
+//  Created by selva on 24/03/26.
+//
+
+import SwiftUI
+
+@main
+struct skills_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ skills-uiTests/skills_uiTests.swift
@@ -0,0 +1,17 @@
+//
+//  skills_uiTests.swift
+//  skills-uiTests
+//
+//  Created by selva on 24/03/26.
+//
+
+import Testing
+@testable import skills_ui
+
+struct skills_uiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
